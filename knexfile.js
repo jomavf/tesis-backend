2fcca5cdc4d2113a686163841be8ad6646ce712d
ZetaGH <jose.veliz.francia@gmail.com>
2020-04-21 23:32:35 -0500

    add knexfile

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      database: process.env.POSTGRES_DATABASE,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    },
+    migrations: {
+      directory: "./src/db/migrations",
+    },
+    seeds: {
+      directory: "./src/db/seeds",
+    },
+    useNullAsDefault: true,
+  },
+};
