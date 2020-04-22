395b02010d28658d9bec840643df33e0e074f662
ZetaGH <jose.veliz.francia@gmail.com>
2020-04-21 23:56:54 -0500

    fix

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,9 @@ require("dotenv").config();
 module.exports = {
   development: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection:
+      "postgres://gllchhjueskjpn:c5338be6a350dcb33c990167e11696df2faf5d439f290c2a216d28e38a3da348@ec2-18-206-84-251.compute-1.amazonaws.com:5432/doo4575l4vg63",
+    searchPath: ["knex", "public"],
     // {
     //   database: process.env.POSTGRES_DATABASE,
     //   host: process.env.POSTGRES_HOST || "localhost",
