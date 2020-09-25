b362f8db9dd82f0e4c6c20a47e3e10abd89ff1d6
JoseVSDS <josev@sdsharp.com>
2020-09-25 18:56:45 -0500

    created account when checkin

--- src/controllers/checkInController.js
+++ src/controllers/checkInController.js
@@ -1,10 +1,21 @@
 const service = require("../services/checkInService");
+const AccountService = require("../services/accountService");
 
 function create(req, res, next) {}
 
 async function createOrUpdate(req, res, next) {
   try {
-    const createdItem = await service.upsert(req.body);
+    const [createdItem] = await service.upsert(req.body);
+    if (createdItem) {
+      const createdAccount = await AccountService.create({
+        total_amount: 0,
+        billing_date_start: createdItem.start_date,
+        billing_date_end: createdItem.end_date,
+        guest_id: createdItem.guest_id,
+      });
+      console.log("creteadItem", JSON.stringify(createdAccount));
+    }
+
     res.status(200).json({
       success: true,
       data: createdItem,
