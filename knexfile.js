d7937cd4e6551c8b15903e7a6e753c2e35caaca3
zetagh <jose.veliz.francia@gmail.com>
2020-11-10 14:26:47 -0500

    fix

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,7 @@ module.exports = {
     connection: {
       host: process.env.POSTGRES_HOST || "localhost",
       user: process.env.POSTGRES_USER,
+      port: 5432,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
     },
@@ -22,7 +23,7 @@ module.exports = {
   production: {
     client: "pg",
     connection:
-      "postgres://gllchhjueskjpn:c5338be6a350dcb33c990167e11696df2faf5d439f290c2a216d28e38a3da348@ec2-18-206-84-251.compute-1.amazonaws.com:5432/doo4575l4vg63",
+      "postgres://gllchhjueskjpn:c5338be6a350dcb33c990167e11696df2faf5d439f290c2a216d28e38a3da348@ec2-18-206-84-251.compute-1.amazonaws.com:5432/doo4575l4vg63?sslmode=disable",
     searchPath: ["knex", "public"],
     migrations: {
       directory: "./src/db/migrations",
